a72459fa2afb7edc7edfc8a9835670fa480d9b3d
prdethxn <ethanmcque07@gmail.com>
2026-09-05 02:30:45 +1000

    Made LogEvent data type

--- src/Shared/LogEvent.cs
+++ src/Shared/LogEvent.cs
@@ -2,10 +2,10 @@
 
 public class LogEvent
 {
-    public int Id { get; set; } = Guid.NewGuid();
-    public string Level { get; set; } = string.Empty();
-    public string Message { get; set; } = string.Empty();
-    public string Source { get; set; } = string.Empty();
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Level { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public string Source { get; set; } = string.Empty;
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-    public Dictionary<string,string> Metadata { get; set; } = new()
+    public Dictionary<string,string> Metadata { get; set; } = new();
 }
